7cae68eece0b85436d005d149d8caa90930bb82e
taniaferdousey24promy <taniaferdousey24promy@gmail.com>
2023-05-21 13:29:41 +0600

    Commiting to get all the toys data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express =require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express ();
 const port = process.env.PORT || 5000;
@@ -35,12 +35,44 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+
+    const toyCollection = client.db('marvelVault').collection('toys');
+    
+    app.get('/toys', async(req,res)=>{
+        const searchQuery = req.query.search;
+        let query = {};
+        let options ={};
+
+
+        
+
+        const result = await toyCollection.find(query,options).toArray();
+
+        res.send(result);
+    })
+    app.get('/toys/:id', async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const options ={
+            projection:{Category:1, subCategories:1},
+        };
+        const result = await toyCollection.findOne(query,options);
+        res.send(result);
+    })
+
+
+
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
